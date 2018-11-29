0c910af11510b23594eb5b08a7f61c83b13c566d
Shinkojiro <james.a.hall@okstate.edu>
2018-11-28 20:32:48 -0600

    New stuff

--- app/src/main/java/com/cis2323/myapplication/DBHelper.java
+++ app/src/main/java/com/cis2323/myapplication/DBHelper.java
@@ -2,9 +2,12 @@ package com.cis2323.myapplication;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -38,7 +41,29 @@ public class DBHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(DB_COLUMN,task);
-        db.insertWithOnConflict(DB_NAME,null, values, SQLiteDatabase.CONFLICT_ABORT);
+        db.insertWithOnConflict(DB_NAME,null, values, SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
+    }
+
+    public ArrayList<String> getTaskList(){
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query(DB_NAME, new String[]{DB_COLUMN}, null, null, null, null, null);
+
+        while(cursor.moveToNext()){
+            int index = cursor.getColumnIndex(DB_COLUMN);
+            taskList.add(cursor.getString(index));
+        }
+
+        cursor.close();
+        db.close();
+
+        return taskList;
+    }
+
+    public void deleteTask(String task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DB_NAME, DB_COLUMN + " =d ?",new String[]{task});
         db.close();
     }
 }
